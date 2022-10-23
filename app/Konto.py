8481b5268bcd838364ddffce18f7af68c27191c3
Julia <j.majewska.714@studms.ug.edu.pl>
2022-10-23 18:56:57 +0200

    LAB02: Add util functions to separate logic.

--- app/Konto.py
+++ app/Konto.py
@@ -1,11 +1,27 @@
 import re
 
 
+def is_promotion_code_correct(promotion_code):
+    if(re.match(r"PROM_[^\s][^\s][^\s]$", promotion_code)):
+        return True
+    return False
+
+def is_year_of_birdth_goof_for_promotion(pesel):
+    year = int(pesel[:2])
+    century = int(pesel[2:4])
+
+    if (year < 60 and century > 20):
+        return True
+    elif(year > 60):
+        return True
+    return False
+
+
 class Konto:
     def __init__(self, imie, nazwisko, pesel, promotion_code=None):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.saldo = promotion_code
+        self.saldo = (promotion_code, pesel)
         self.pesel = pesel
 
     @property
@@ -24,10 +40,14 @@ class Konto:
         return self._saldo
 
     @saldo.setter
-    def saldo(self, promotion_code):
-        if (promotion_code and re.match(r"PROM_[^\s][^\s][^\s]$", promotion_code)):
+    def saldo(self, args):
+        promotion_code, pesel = args
+        if (promotion_code and is_promotion_code_correct(promotion_code) and is_year_of_birdth_goof_for_promotion(pesel)):
             self._saldo = 50
         else:
             self._saldo = 0
 
 
+# konto = Konto("J", "M", "61000000000", "PROM_XYZ")
+# print(konto.saldo)
+print(is_promotion_code_correct("PROM_XYZ"))
