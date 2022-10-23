98c227a70132a93af9c306b9dcde2110ed093783
Julia <j.majewska.714@studms.ug.edu.pl>
2022-10-23 15:50:36 +0200

    LAB02: Add implementation for promotion code.

--- app/Konto.py
+++ app/Konto.py
@@ -1,19 +1,33 @@
+import re
+
+
 class Konto:
-    def __init__(self, imie, nazwisko, pesel):
+    def __init__(self, imie, nazwisko, pesel, promotion_code=None):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.saldo = 0
+        self.saldo = promotion_code
         self.pesel = pesel
-    
+
     @property
     def pesel(self):
         return self._pesel
-    
+
     @pesel.setter
-    def pesel(self, pesel): 
-        if(len(pesel) != 11):
+    def pesel(self, pesel):
+        if (len(pesel) != 11):
             self._pesel = "Niepoprawny pesel!"
-        else: 
+        else:
             self._pesel = pesel
-    
+
+    @property
+    def saldo(self):
+        return self._saldo
+
+    @saldo.setter
+    def saldo(self, promotion_code):
+        if (promotion_code and re.match(r"PROM_[^\s][^\s][^\s]$", promotion_code)):
+            self._saldo = 50
+        else:
+            self._saldo = 0
+
 
