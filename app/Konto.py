be8534046d74e4bd07e444cbe3442eb5166696fb
Julia <j.majewska.714@studms.ug.edu.pl>
2022-10-23 15:04:28 +0200

    LAB02: Add functionality to validate and set pesel in Konto class.

--- app/Konto.py
+++ app/Konto.py
@@ -1,6 +1,19 @@
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, imie, nazwisko, pesel):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
-        pass
+        self.pesel = pesel
+    
+    @property
+    def pesel(self):
+        return self._pesel
+    
+    @pesel.setter
+    def pesel(self, pesel): 
+        if(len(pesel) != 11):
+            self._pesel = "Niepoprawny pesel!"
+        else: 
+            self._pesel = pesel
+    
+
