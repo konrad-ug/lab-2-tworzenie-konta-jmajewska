0d8b6182deae35f0a93e32e6dfc8157ff2b24bb6
Julia <j.majewska.714@studms.ug.edu.pl>
2022-10-27 17:08:46 +0200

    LAB03: Add test for positive test case for sending transfers.

---
+++ app/tests/test_transfers.py
@@ -0,0 +1,16 @@
+import unittest
+from ..Konto import Konto
+
+class TestTransfers(unittest.TestCase):
+    mocked_name = "Name"
+    mocked_surname = "Surname"
+    mocked_amount = 800
+    mocked_saldo_too_little = 100
+    mocked_saldo_enough = 1000
+    mocked_pesel = "61200000000"
+
+    def test_recieve_transfer_correct(self):
+        konto = Konto(self.mocked_name, self.mocked_surname, self.mocked_pesel)
+        konto._saldo = self.mocked_saldo_enough
+        konto.send(self.mocked_amount)
+        self.assertEqual(konto.saldo, self.mocked_saldo_enough - self.mocked_amount)
