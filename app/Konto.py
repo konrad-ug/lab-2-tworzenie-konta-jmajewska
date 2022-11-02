b86b3679fe5c8cb4ac55b95380377c0e8789c3df
Julia <j.majewska.714@studms.ug.edu.pl>
2022-11-02 09:07:41 +0100

    LAB03: Modify Konto class to handle both - private and company accounts fast transfers.

--- app/Konto.py
+++ app/Konto.py
@@ -2,9 +2,10 @@ import re
 
 
 def is_promotion_code_correct(promotion_code):
-    if(re.match(r"PROM_[^\s][^\s][^\s]$", promotion_code)):
+    if (re.match(r"PROM_[^\s][^\s][^\s]$", promotion_code)):
         return True
     return False
+
 
 def is_year_of_birdth_goof_for_promotion(pesel):
     year = int(pesel[:2])
@@ -12,9 +13,17 @@ def is_year_of_birdth_goof_for_promotion(pesel):
 
     if (year < 60 and century > 20):
         return True
-    elif(year > 60):
+    elif (year > 60):
         return True
     return False
+
+
+class KontoBase:
+    def __init__(self):
+        pass
+    
+    isPrivate = False
+    isFirm = False
 
 
 class Konto:
@@ -46,14 +55,48 @@ class Konto:
             self._saldo = 50
         else:
             self._saldo = 0
-    
-    def send(self,amount):
+
+    def send(self, amount):
         saldo_after_transfer = self.saldo - amount
-        if(saldo_after_transfer < 0):
+        if (saldo_after_transfer < 0):
             return False
         else:
-            self._saldo = saldo_after_transfer     
-            return True       
+            self._saldo = saldo_after_transfer
+            return True
 
     def recieve(self, amount):
         self._saldo = self.saldo + amount
+
+    def send_fast(self, amount):
+        if(isinstance(self, KontoFirmowe)):
+            charge = 5
+        else:
+            charge = 1
+
+        saldo_after_transfer = self.saldo - amount - charge
+
+        if(saldo_after_transfer < -charge):
+            return False
+        else: 
+            self._saldo = saldo_after_transfer
+
+
+class KontoFirmowe(Konto):
+    def __init__(self,firm_name,nip,imie="",nazwisko="",pesel=""):
+        super().__init__(self,imie,nazwisko,pesel)
+        self.firm_name = firm_name
+        self.nip = (nip)
+
+    @property
+    def nip(self):
+        return self._nip
+
+    @nip.setter
+    def nip(self,nip):
+        if(len(nip) != 10):
+            self._nip = "Niepoprawny nip"
+        else: 
+            self._nip = nip
+    
+
+konto = KontoFirmowe("k","p")
